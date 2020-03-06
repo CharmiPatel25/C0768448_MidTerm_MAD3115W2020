c67013d6afe5eb38568adbc6b6dac5f319eda34e
charmi <c0768448@mylambton.ca>
2020-03-05 20:28:37 -0500

    [CP] Add login fields

--- C0768448_MidTerm_MAD3115W2020/C0768448_MidTerm_MAD3115W2020/LoginViewController.swift
+++ C0768448_MidTerm_MAD3115W2020/C0768448_MidTerm_MAD3115W2020/LoginViewController.swift
@@ -8,8 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
+    @IBOutlet weak var txtEmailId: UITextField!
+    
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    @IBOutlet weak var RememberMe: UISwitch!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
