008522f2bc73d65b00d4f5fe644563476d906df3
f <kaurmandeep239@gmail.com>
2020-03-07 13:43:19 -0500

    [CP] Add new files

---
+++ C0768448_MidTerm_MAD3115W2020/C0768448_MidTerm_MAD3115W2020/Customer.swift
@@ -0,0 +1,9 @@
+//
+//  Customer.swift
+//  C0768448_MidTerm_MAD3115W2020
+//
+//  Created by MacStudent on 2020-03-07.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
