c31ef989e792515e2a820c63eaa733dcf600ba7b
charmi <C0768448@mylambton.ca>
2020-03-10 18:43:11 -0400

    [CP] Add Display protocol

---
+++ C0768448_MidTerm_MAD3115W2020/C0768448_MidTerm_MAD3115W2020/IDisplay.swift
@@ -0,0 +1,12 @@
+//
+//  IDisplay.swift
+//  C0768448_MidTerm_MAD3115W2020
+//
+//  Created by MacStudent on 2020-03-10.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+protocol IDisplay {
+    func Display()
+}
