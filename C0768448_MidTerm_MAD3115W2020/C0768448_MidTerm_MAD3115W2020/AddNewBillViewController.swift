9dd59934b2326a502f97fb1f605a74adce39d53b
charmi <c0768448@mylambton.ca>
2020-03-13 19:19:39 -0400

    [CP] Add new bill

--- C0768448_MidTerm_MAD3115W2020/C0768448_MidTerm_MAD3115W2020/AddNewBillViewController.swift
+++ C0768448_MidTerm_MAD3115W2020/C0768448_MidTerm_MAD3115W2020/AddNewBillViewController.swift
@@ -13,22 +13,24 @@ class AddNewBillViewController: UIViewController {
     @IBOutlet weak var segBillType: UISegmentedControl!
     @IBOutlet weak var txtBillID: UITextField!
     @IBOutlet weak var txtBillDate: UITextField!
-    @IBOutlet weak var txtMobileMins: UITextField!
-    @IBOutlet weak var txtMobileData: UITextField!
-    @IBOutlet weak var txtMobileNumber: UITextField!
-    @IBOutlet weak var txtMobileManufacture: UITextField!
+   
+    @IBOutlet weak var txtInternetDataUsed: UITextField!
+    @IBOutlet weak var txtInternetProvider: UITextField!
+    @IBOutlet weak var txtMobileManifature: UITextField!
+    
+    @IBOutlet weak var txtHydroUnitsUsed: UITextField!
     @IBOutlet weak var txtMobilePlan: UITextField!
     @IBOutlet weak var txtHydroProvider: UITextField!
-    @IBOutlet weak var txtHydroUnits: UITextField!
-    @IBOutlet weak var txtInternetProvider: UITextField!
-    @IBOutlet weak var txtInternetData: UITextField!
     
+    @IBOutlet weak var txtMobileNumber: UITextField!
     
+    @IBOutlet weak var txtMobileDataUsed: UITextField!
+    
+    @IBOutlet weak var txtMinutesUsed: UITextField!
     var selectedCustomer: Customer?
        var currentSelected: Int = 0
        var datePicker : UIDatePicker!
        var pickerView : UIPickerView!
-       var billType = ["Mobile" , "Internet" , "Hydro"]
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.hidesBackButton = true
@@ -49,7 +51,7 @@ class AddNewBillViewController: UIViewController {
         if (segBillType.selectedSegmentIndex == 0){
                    
                        if((txtBillID.text?.contains("MB"))!){
-                        let bill = Mobile(billId: txtBillID.text!, billDate: (txtBillDate.text?.toDate())!, billType: BillType.Mobile, manufacturerName: txtMobileManufacture.text!, planName: txtMobilePlan.text!, mobileNumber: txtMobileNumber.text!, mobGbUsed: Int(txtMobileData!.text!)!, minute: Int(txtMobileMins!.text!)!)
+                        let bill = Mobile(billId: txtBillID.text!, billDate: (txtBillDate.text?.toDate())!, billType: BillType.Mobile, manufacturerName: txtMobileManifature.text!, planName: txtMobilePlan.text!, mobileNumber: txtMobileNumber.text!, mobGbUsed: Int(txtMobileDataUsed!.text!)!, minute: Int(txtMinutesUsed!.text!)!)
                     selectedCustomer?.newBill(bill: bill, billId: txtBillID.text!)
                            billAdded()
                        }
@@ -64,7 +66,7 @@ class AddNewBillViewController: UIViewController {
                }
                if(segBillType.selectedSegmentIndex == 1){
                    if((txtBillID.text?.contains("HY"))!){
-                    let bill = Hydro(billId: txtBillID.text!, billDate: (txtBillDate.text?.toDate())!, billType: BillType.Hydro, agencyName: txtHydroProvider.text!, unitsUsed: Double(txtHydroUnits!.text!)!)
+                    let bill = Hydro(billId: txtBillID.text!, billDate: (txtBillDate.text?.toDate())!, billType: BillType.Hydro, agencyName: txtHydroProvider.text!, unitsUsed: Double(txtHydroUnitsUsed!.text!)!)
                     
                    selectedCustomer?.newBill(bill: bill, billId: txtBillID.text!)
                    billAdded()
@@ -78,7 +80,7 @@ class AddNewBillViewController: UIViewController {
                }
                if(segBillType.selectedSegmentIndex == 2){
                    if((txtBillID.text?.contains("IN"))!){
-                    let bill = Internet(billId: txtBillID.text!, billDate: (txtBillDate.text?.toDate())!, billType: BillType.Internet, providerName: txtInternetProvider.text!, gbUsed: Double(txtInternetData!.text!)!)
+                    let bill = Internet(billId: txtBillID.text!, billDate: (txtBillDate.text?.toDate())!, billType: BillType.Internet, providerName: txtInternetProvider.text!, gbUsed: Double(txtInternetDataUsed!.text!)!)
                    selectedCustomer?.newBill(bill: bill, billId: txtBillID.text!)
                    billAdded()
                    }
@@ -112,46 +114,46 @@ class AddNewBillViewController: UIViewController {
     }
     
     func hideTextFirst(){
-    self.txtMobileMins.isHidden = true
-    self.txtMobileData.isHidden = true
-    self.txtMobileManufacture.isHidden = true
+    self.txtMinutesUsed.isHidden = true
+    self.txtMobileDataUsed.isHidden = true
+    self.txtMobileManifature.isHidden = true
     self.txtMobilePlan.isHidden = true
     self.txtMobileNumber.isHidden = true
-    self.txtMobileManufacture.isHidden = false
+    self.txtMobileManifature.isHidden = false
     }
     func hideText(){
-        self.txtHydroUnits.isHidden = true;
+        self.txtHydroUnitsUsed.isHidden = true;
         self.txtHydroProvider.isHidden = true;
-        self.txtInternetData.isHidden = true
+        self.txtInternetDataUsed.isHidden = true
         self.txtInternetProvider.isHidden = true
-        self.txtMobileMins.isHidden = false
-        self.txtMobileData.isHidden = false
-        self.txtMobileManufacture.isHidden = false
+        self.txtMinutesUsed.isHidden = false
+        self.txtMobileDataUsed.isHidden = false
+        self.txtMobileManifature.isHidden = false
         self.txtMobilePlan.isHidden = false
         self.txtMobileNumber.isHidden = false
-        self.txtMobileManufacture.isHidden = false
+        self.txtMobileManifature.isHidden = false
     }
     
     @IBAction func segmentBillType(_ sender: Any) {
         
         if (segBillType.selectedSegmentIndex == 0) {
-                    self.txtHydroUnits.isHidden = true;
+                    self.txtHydroUnitsUsed.isHidden = true;
                     self.txtHydroProvider.isHidden = true;
                     hideText()
                 }
                 if(segBillType.selectedSegmentIndex == 1) {
                     hideTextFirst()
                     self.txtHydroProvider.isHidden = false
-                    self.txtHydroUnits.isHidden = false
-                    self.txtInternetData.isHidden = true
+                    self.txtHydroUnitsUsed.isHidden = false
+                    self.txtInternetDataUsed.isHidden = true
                     self.txtInternetProvider.isHidden = true
       
                 }
                 if(segBillType.selectedSegmentIndex == 2){
                      hideTextFirst()
                     self.txtHydroProvider.isHidden = true
-                    self.txtHydroUnits.isHidden = true
-                    self.txtInternetData.isHidden = false
+                    self.txtHydroUnitsUsed.isHidden = true
+                    self.txtInternetDataUsed.isHidden = false
                     self.txtInternetProvider.isHidden = false
     }
     
